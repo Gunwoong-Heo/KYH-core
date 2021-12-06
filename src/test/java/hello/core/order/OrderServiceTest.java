9acea25e35885a694dcd67444e726dde937e5b71
Gunwoong-Heo <gunwoong1129@gmail.com>
2021-12-06 22:27:30 +0900

    OrderServiceTest 추가

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,18 +1,30 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
+    OrderService orderService;
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        orderService = appConfig.orderService();
+        memberService = appConfig.memberService();
+    }
+
     @Test
     void createOrder() {
-        OrderService orderService = new OrderServiceImpl();
-        MemberService memberService = new MemberServiceImpl();
+//        OrderService orderService = new OrderServiceImpl();
+//        MemberService memberService = new MemberServiceImpl();
 
         Long memberId = 1L;
         Member member = new Member(1L, "memberA", Grade.VIP);
